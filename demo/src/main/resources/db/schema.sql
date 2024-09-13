1ba9f3f047002987274e382634cc63aebc71b5c6
Habist <liasn@naver.com>
2024-09-13 13:07:11 +0900

    [refactor] schema updated

--- demo/src/main/resources/db/schema.sql
+++ demo/src/main/resources/db/schema.sql
@@ -2,11 +2,11 @@ create table destination
 (
     id             bigint auto_increment
         primary key,
-    ct_prvn_name   varchar(255)   null,
-    description    varchar(255)   null,
-    name           varchar(255)   null,
-    si_gun_gu_name varchar(255)   null,
-    point          varbinary(255) null,
+    ct_prvn_name   varchar(255)  not null,
+    description    text   null,
+    name           varchar(255)  not null,
+    si_gun_gu_name varchar(255)  not null,
+    point          varbinary(255) not null,
     index('ct_prvn_name', 'si_gun_gu_name')
 );
 
